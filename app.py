58345abac28ef7a2a2bca0b8509abf7367607557
Ubuntu <ubuntu@ip-172-31-45-255.ap-southeast-2.compute.internal>
2020-08-15 10:31:33 +0000

    user status check on login

--- app.py
+++ app.py
@@ -23,38 +23,53 @@ def login():
     msg = ''
     
     #Check form has been submitted
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'accountid' in request.form and 'password' in request.form:
         #username and password from form into variable
-        username = request.form['username']
+        accountid = request.form['accountid']
         password = request.form['password']
 
         #check db for user & password
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+        cursor.execute('SELECT * FROM accounts WHERE accountid = %s AND password = %s', (accountid, password))
         #fetch the record
         user = cursor.fetchone()
         
         #if account exsists
         if user:
-            #make user exsist in some session data
-            session['islogged'] = True
-            session['username'] = user['username']
-            #move user to home page (index)
-            return redirect(url_for('home'))
+            #check user has not been disabled
+            if user['accountStatus'] == 1:
+                #make user exsist in some session data
+                session['islogged'] = True
+                session['accountid'] = user['accountid']
+                session['name'] = user['name']
+                session['accountTypeID'] = user['accountTypeID']
+                #move user to home page (index)
+                return redirect(url_for('home'))
+            else:
+                user = ''
+                msg = 'user has been disabled by an admin'
+                return render_template('login.html', msg=msg) 
         
         else:
             #account does not exsit in DB reject
             msg = 'user does not exsist please contact someone who cares'
             
-    return render_template('login.html', msg='')
+    return render_template('login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return render_template('login.html', msg = 'logged out')
 
 @app.route('/home')
 def home():
     if 'islogged' in session:
         #user is logged in keep them around
         #pass through values
-        username = session['username']
-        return render_template('index.html', username = username )
+        name = session['name']
+        print(session)
+        return render_template('index.html', name = name )
     else:
         #return to login screen
         return redirect(url_for('login'))
